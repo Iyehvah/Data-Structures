021c80946bf73dbe9f8bbf65b259062ef011b63b
Iyehvah <Iyehvah@users.noreply.github.com>
2020-06-22 13:16:21 -0500

    mvp met for module 2

--- queue/queue.py
+++ queue/queue.py
@@ -9,25 +9,31 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
+   "Same thing as stack except your pretty much just adding to the head and removing from the tail
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+import sys 
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
     
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def enqueue(self, value):
-        self.storage.insert(0, value)
         self.size += 1
+        self.storage.add_to_tail(value)
             
     def dequeue(self):
-        if len(self.storage) > 0:
-            self.size -= 1
-            return self.storage.pop()
-        else:
-            return None 
+        if self.size == 0:
+            return None
+        self.size -= 1
+        value = self.storage.remove_head()
+        return value
